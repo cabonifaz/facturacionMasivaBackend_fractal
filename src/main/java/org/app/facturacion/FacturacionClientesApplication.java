1e9643ef6fc2374a3c4b4c2a0d5eb5d4ef993125
zero <himura.js@hotmail.com>
2025-11-24 13:31:33 -0500

    First commit

---
+++ src/main/java/org/app/facturacion/FacturacionClientesApplication.java
@@ -0,0 +1,13 @@
+package org.app.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionClientesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/app/facturacion/infrastructure/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.app.facturacion.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping("/")
+  public String home() {
+    return "API FactClientBk is working";
+  }
+}
