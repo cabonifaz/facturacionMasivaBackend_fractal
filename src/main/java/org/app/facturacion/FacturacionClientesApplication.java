0f7652d10e808fa9fdd536fd08ef7bd909689f83
zero <himura.js@hotmail.com>
2025-12-12 18:10:47 -0500

    feat: Async mode enabled

--- src/main/java/org/app/facturacion/FacturacionClientesApplication.java
+++ src/main/java/org/app/facturacion/FacturacionClientesApplication.java
@@ -2,7 +2,9 @@ package org.app.facturacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class FacturacionClientesApplication {
 
