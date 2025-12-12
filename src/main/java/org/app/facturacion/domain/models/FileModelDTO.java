b8da62bf6d5746fad58e3a5362949cc9bb12fa8d
zero <himura.js@hotmail.com>
2025-12-12 13:12:37 -0500

    refractor: Update ClassName

---
+++ src/main/java/org/app/facturacion/domain/models/FileModelDTO.java
@@ -0,0 +1,28 @@
+package org.app.facturacion.domain.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FileModelDTO {
+
+  private String filename;
+  private byte[] fileBytes;
+  private String fileType;
+
+  @Override
+  public String toString() {
+
+    return new StringBuilder()
+        .append("Filename: ")
+        .append(this.filename)
+        .append("Size: ")
+        .append(fileBytes.length)
+        .append(" bytes")
+        .append("MediaType: ")
+        .append(this.fileType)
+        .toString();
+  }
+
+}
